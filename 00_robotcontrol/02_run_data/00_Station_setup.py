c598c98fc12174fceca264ca9ae720e15bab2da7
Jomana B <jumanabaddad@gmail.com>
2021-04-27 13:03:34 +0200

    tests

--- 00_robotcontrol/02_run_data/00_Station_setup.py
+++ 00_robotcontrol/02_run_data/00_Station_setup.py
@@ -33,9 +33,9 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) > 1:
-        filepath = "00_location.json"
+        filepath = "xx_Test_cut_2.json"
     else:
-        print("No input file specified, using example file 00_location.json")
-        filepath = os.path.abspath(os.path.join(__file__, "..", "00_location.json"))
+        print("No input file specified, using example file xx_Test_cut_2")
+        filepath = os.path.abspath(os.path.join(__file__, "..", "xx_Test_cut_2.json"))
 
     run_marking(filepath)
